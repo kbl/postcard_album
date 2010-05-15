c43c8ab4f5f6b3f01bf7b1b5bef46817e2c89a5e
kbl <m.pietraszek@gmail.com>
2010-05-15 20:43:31 +0200

    jquery for dom manipulation

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,12 +1,10 @@
 function remove_image(link) {
-  $(link).previous("input[type=hidden]").value = "1";
-  $(link).up(".image_fields").hide();
+  $(link).prev("input[type=hidden]").val("1");
+  $(link).closest(".image_fields").hide();
 }
 
 function add_image_fields(link, content) {
   var new_id = new Date().getTime();
   var regexp = new RegExp("new_image", "g");
-  $(link).up().insert({
-    before: content.replace(regexp, new_id)
-  }); 
+  $(link).parent().before(content.replace(regexp, new_id)); 
 }
