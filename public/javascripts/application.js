d24514e1b1729d5a9a87722daae96ada4218b48b
kbl <m.pietraszek@gmail.com>
2010-05-23 22:14:29 +0200

    better postcard presentation

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -9,11 +9,46 @@ function add_image_fields(link, content) {
   $(link).parent().before(content.replace(regexp, new_id)); 
 }
 
-// function used with google maps
+// functions used with google maps
 function createMarker(point, html) {
   var marker = new GMarker(point);
   GEvent.addListener(marker, "click", function() {
     marker.openInfoWindowHtml(html);
   });
   return marker;
+}
+
+function drawGoogleMap(latitude, longitude) {
+  if (GBrowserIsCompatible()) { 
+    var map = new GMap2(document.getElementById("map"));
+    map.addControl(new GLargeMapControl());
+    map.addControl(new GMapTypeControl());
+    latLng = new GLatLng(latitude, longitude)
+    map.setCenter(latLng, 16);
+    map.enableScrollWheelZoom(); 
+    map.addOverlay(new GMarker(latLng));
+  }
+}
+
+// function used to set up a shadowbox in _postcard template
+function setUpShadowbox() {
+  Shadowbox.init(
+    { skipSetup: true}, 
+    function() {
+      Shadowbox.setup(
+        'a.lightbox', 
+        { gallery: 'postcards',
+          continuous: true})
+  });
+}
+
+// function usetd on main page with slider
+function setUpSlider() {
+  $('#slider').easySlider({
+    auto: true,
+    continuous: true,
+    controlsShow: false,
+    speed: 800,
+    pause: 2400
+  });
 }
