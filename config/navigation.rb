4fea8e4c480bdaae8b92e8a297b5185625491908
kbl <m.pietraszek@gmail.com>
2010-06-16 21:38:05 +0200

    complete i18n, currently only polish translation

--- config/navigation.rb
+++ config/navigation.rb
@@ -2,10 +2,10 @@ SimpleNavigation::Configuration.run do |navigation|
 
   navigation.items do |primary|
     primary.dom_class = 'sf-menu clear'
-    primary.item :index, content_tag(:span, 'strona główna'), '/'
-    primary.item :postcards, content_tag(:span, 'widokówki'), postcards_path
-    primary.item :contact, content_tag(:span, 'odnośniki'), '/a'
-    primary.item :contact, content_tag(:span, 'kontakt'), '/b'
+    primary.item :index, content_tag(:span, t(:nav_main_page)), '/'
+    primary.item :postcards, content_tag(:span, t(:nav_postcards)), postcards_path
+    primary.item :contact, content_tag(:span, t(:nav_links)), '/a'
+    primary.item :contact, content_tag(:span, t(:nav_contact)), '/b'
   end
   
 end
