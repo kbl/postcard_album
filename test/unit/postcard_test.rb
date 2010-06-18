183f3c72604bdcdb6eb81c42192e46d6c4957dc8
kbl <m.pietraszek@gmail.com>
2010-06-18 21:28:05 +0200

    wrong javascript, stylesheet include tags

--- test/unit/postcard_test.rb
+++ test/unit/postcard_test.rb
@@ -2,14 +2,6 @@ require 'test_helper'
 
 class PostcardTest < ActiveSupport::TestCase
  
-  test "should return paginated results" do
-    (Postcard.per_page + 1).times do
-      Factory(:postcard)
-    end
-
-    assert Postcard.find_all_paginated(page_number = 1).size == Postcard.per_page, "should return collection with size of page results" 
-  end 
-
   test "should not save poscard with year before 1800" do
     postcard = Factory.build(:postcard, :year => 1799)
     
