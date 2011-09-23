e9ad8ccca459dd1f51233e5ac724f7d0b0b3fce9
kbl <m.pietraszek@gmail.com>
2011-09-23 21:57:33 +0200

    excplicit name of js file, additional conf for slider

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,5 +1,21 @@
 (function (){
     $(document).ready(function() {
-        $('#slider').nivoSlider();
+        $('#slider').nivoSlider({
+            effect: 'fade', // Specify sets like: 'fold,fade,sliceDown'
+            animSpeed: 500, // Slide transition speed
+            pauseTime: 3000, // How long each slide will show
+            startSlide: rand, // Set starting Slide (0 index)
+            directionNav: true, // Next & Prev navigation
+            directionNavHide: true, // Only show on hover
+            controlNav: true, // 1,2,3... navigation
+            controlNavThumbs: false, // Use thumbnails for Control Nav
+            controlNavThumbsFromRel: true, // Use image rel for thumbs
+            keyboardNav: true, // Use left & right arrows
+            pauseOnHover: true, // Stop animation while hovering
+            manualAdvance: false, // Force manual transitions
+            captionOpacity: 0.8, // Universal caption opacity
+            prevText: 'Prev', // Prev directionNav text
+            nextText: 'Next'
+        });
     });
 })();
