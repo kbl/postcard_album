7a1e9d1af29b403d830494f0c789868105288401
kbl <m.pietraszek@gmail.com>
2011-09-23 21:23:04 +0200

    nivo slider, full content

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,8 +4,9 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
 //
-//= require analytics
 //= require jquery
 //= require jquery_ujs
+//= require analytics
+//= require nivo/jquery.nivo.slider.pack
 //= require shadowbox
 //= require_tree .
--- app/assets/javascripts/maps-api.js
+++ app/assets/javascripts/maps-api.js
@@ -1,15 +1,3 @@
-function removeImage(link) {
-  $(link).prev("input[type=hidden]").val("1");
-  $(link).closest(".image_fields").hide();
-}
-
-function addImageFields(link, content) {
-  var new_id = new Date().getTime();
-  var regexp = new RegExp("new_image", "g");
-  $(link).parent().before(content.replace(regexp, new_id));
-}
-
-// functions used with google maps
 function createMarker(point, html) {
   var marker = new GMarker(point);
   GEvent.addListener(marker, "click", function() {
