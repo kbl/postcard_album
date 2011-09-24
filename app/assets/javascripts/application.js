f71a7ab9dcc0611f9da1ce6b12e40fae901cd539
kbl <m.pietraszek@gmail.com>
2011-09-24 14:41:48 +0200

    jquery plugin for better tag handling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,10 +4,12 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
 //
-//= require jquery
+//= require jquery.min
+//= require jquery-ui.min
 //= require jquery_ujs
 //= require analytics
 //= require nivo/jquery.nivo.slider.pack
 //= require shadowbox
 //= require maps-api
 //= require nested-form
+//= require jquery.tagsinput.min
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,10 +1,14 @@
 class TagsController < ApplicationController
 
   def index
-    @postcards = Postcard.tagged_with(params[:id]).paginate(:page => params[:page], :order => 'created_at DESC')
-    @publishers = Publisher.find_all_combobox || []
+    if params[:id]
+      @postcards = Postcard.tagged_with(params[:id]).paginate(:page => params[:page], :order => 'created_at DESC')
+      @publishers = Publisher.find_all_combobox || []
 
-    render :template => 'postcards/index'
+      render template: 'postcards/index'
+    else
+      render json: @tags
+    end
   end
 
 end
