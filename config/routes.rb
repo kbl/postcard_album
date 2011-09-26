75f145fb105569b5fb57fdaebf644f82bc51c654
kbl <m.pietraszek@gmail.com>
2011-09-26 18:59:12 +0200

    localized routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,72 +1,21 @@
 PostcardAlbum::Application.routes.draw do
 
-  # TODO Rails3
-  match :login, :controller => :user_sessions, :action => :new
-  match :logout, :controller => :user_sessions, :action => :destroy
+  match 'login' => 'user_sessions#new'
+  match 'logout' => 'user_sessions#destroy'
 
+  match '/publishers/:id' => redirect '/wydawcy/%{id}'
+  match '/postcards/:id' => redirect '/widokowki/%{id}'
+  match '/tags/:id' => redirect '/tagi/%{id}'
+
+  resources :users, path: 'uzytkownicy'
   resources :user_sessions
-  resources :users
-  resources :publishers
-  resources :postcards
-  resources :postcard_sets
-  resources :index
-  resources :tags
-  resources :newsletter_accounts
+  resources :publishers, path: 'wydawcy'
+  resources :postcards, path: 'widokowki'
+  resources :tags, path: 'tagi'
 
-  match '/postcards/images/:id/:style.:format' => 'images#show'
+  match '/postcards/images/:id/:style.:format' => redirect '/widokowki/zdjecia/%{id}/%{style}.%{format}'
+  match '/widokowki/zdjecia/:id/:style.:format' => 'images#show'
+
   root :to => 'index#index'
-
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  match ':controller(/:action(/:id(.:format)))'
 
 end
