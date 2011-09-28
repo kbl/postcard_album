e47b74716f4c02521bcb72be397d0b7f825c9127
kbl <m.pietraszek@gmail.com>
2011-09-28 15:14:54 +0200

    updating capistrano configration

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,20 +1,29 @@
 set :user do
   Capistrano::CLI.ui.ask "Give me Megiteam username: "
 end
+set :remote_home, "/home/#{user}/www"
+set :db_config_location, "#{remote_home}/config/database.yml"
+set :environment_settings_location, "#{remote_home}/config/.environment"
+
 set :application, 'postcard_album'
 set :repository,  'git://github.com/kbl/postcard_album.git'
-set :deploy_to, "/home/#{user}/www/cap_postcard_album/"
+set :deploy_to, "#{remote_home}/cap_postcard_album/"
 set :use_sudo, false
 
 set :scm, :git
 
 role :app, "#{user}.megiteam.pl"
-role :db,  "sql.#{user}.megiteam.pl", :no_release => true, :primary => true
+role :web, "#{user}.megiteam.pl"
+role :db,  "sql.#{user}.megiteam.pl", no_release: true, primary: true
 
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :update_configuration, except: {no_relase: true} do
+    run <<-CMD
+      cp -vf #{db_config_location} #{release_path}/config/database.yml
+      cp -vf #{environment_settings_location} #{release_path}/.environment
+    CMD
+  end
+
+  after 'deploy:setup', 'deploy:update_configuration'
+end
+
