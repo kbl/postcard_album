5cf0ffee0d3a8b17fe375d5ad937537f71d65e63
kbl <m.pietraszek@gmail.com>
2011-10-06 07:16:21 +0200

    menu item, better postcard set presentation

--- config/navigation.rb
+++ config/navigation.rb
@@ -4,6 +4,7 @@ SimpleNavigation::Configuration.run do |navigation|
     primary.dom_class = 'sf-menu clear'
     primary.item :index, content_tag(:span, t(:nav_main_page)), root_path
     primary.item :postcards, content_tag(:span, t(:nav_postcards)), postcards_path
+    primary.item :postcard_sets, content_tag(:span, 'Zestawy'), postcard_sets_path
     primary.item :publishers, content_tag(:span, t(:nav_publishers)), publishers_path
     primary.item :contact, content_tag(:span, 'Kontakt'), contact_path
   end
