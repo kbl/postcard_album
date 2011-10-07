d806e11b1a2ceb785cd591925129df52e985b1c8
kbl <m.pietraszek@gmail.com>
2011-10-07 13:57:24 +0200

    modified schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111006090949) do
+ActiveRecord::Schema.define(:version => 20111007080607) do
 
   create_table "images", :force => true do |t|
     t.string   "type_of_image"
@@ -33,11 +33,11 @@ ActiveRecord::Schema.define(:version => 20111006090949) do
   end
 
   create_table "postcard_sets", :force => true do |t|
-    t.string   "description"
     t.boolean  "show_abverse"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "title"
+    t.text     "description"
   end
 
   create_table "postcards", :force => true do |t|
