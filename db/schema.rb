5b6c1a5c84691ba65a63e7bf547a53f443366ebc
Marcin Pietraszek <m.pietraszek@gmail.com>
2017-12-19 19:13:24 +0100

    New column used by tags

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -9,86 +8,86 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111007080607) do
+ActiveRecord::Schema.define(version: 20171219180751) do
 
-  create_table "images", :force => true do |t|
-    t.string   "type_of_image"
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.integer  "postcard_id"
+  create_table "images", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "type_of_image"
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.integer "postcard_id"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "image_updated_at"
   end
 
-  create_table "postcard_set_elements", :force => true do |t|
-    t.integer  "order_number"
-    t.integer  "postcard_id"
-    t.integer  "postcard_set_id"
+  create_table "postcard_set_elements", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "order_number"
+    t.integer "postcard_id"
+    t.integer "postcard_set_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "postcard_sets", :force => true do |t|
-    t.boolean  "show_abverse"
+  create_table "postcard_sets", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.boolean "show_abverse"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "title"
-    t.text     "description"
+    t.string "title"
+    t.text "description"
   end
 
-  create_table "postcards", :force => true do |t|
-    t.string   "name"
-    t.text     "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.boolean  "is_horizontal"
-    t.string   "latitude"
-    t.string   "longitude"
-    t.integer  "year"
-    t.integer  "publisher_id"
-    t.boolean  "showable_on_main"
-  end
-
-  create_table "publishers", :force => true do |t|
-    t.string   "name"
-    t.text     "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "taggings", :force => true do |t|
-    t.integer  "tag_id"
-    t.integer  "taggable_id"
-    t.integer  "tagger_id"
-    t.string   "tagger_type"
-    t.string   "taggable_type"
-    t.string   "context"
-    t.datetime "created_at"
-  end
-
-  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
-  add_index "taggings", ["taggable_id", "taggable_type", "context"], :name => "index_taggings_on_taggable_id_and_taggable_type_and_context"
-
-  create_table "tags", :force => true do |t|
+  create_table "postcards", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
+    t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.boolean "is_horizontal"
+    t.string "latitude"
+    t.string "longitude"
+    t.integer "year"
+    t.integer "publisher_id"
+    t.boolean "showable_on_main"
   end
 
-  create_table "user_sessions", :force => true do |t|
-    t.string   "username"
-    t.string   "password"
+  create_table "publishers", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name"
+    t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "users", :force => true do |t|
-    t.string   "username"
-    t.string   "crypted_password"
-    t.string   "password_salt"
-    t.string   "persistence_token"
+  create_table "taggings", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "tag_id"
+    t.integer "taggable_id"
+    t.integer "tagger_id"
+    t.string "tagger_type"
+    t.string "taggable_type"
+    t.string "context"
+    t.datetime "created_at"
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+    t.index ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context"
+  end
+
+  create_table "tags", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name"
+    t.integer "taggings_count", default: 0
+  end
+
+  create_table "user_sessions", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "username"
+    t.string "password"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "username"
+    t.string "crypted_password"
+    t.string "password_salt"
+    t.string "persistence_token"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
